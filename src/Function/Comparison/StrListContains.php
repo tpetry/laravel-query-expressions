ee76148b07051dce6c94f17d8fdaa55ee39e7c17
tpetry <github@tpetry.me>
2023-06-09 10:55:27 +0200

    fix: sqlite

--- src/Function/Comparison/StrListContains.php
+++ src/Function/Comparison/StrListContains.php
@@ -29,7 +29,8 @@ class StrListContains implements ConditionExpression
         // PostgreSQL: The string_to_array is not used because citext values would be cast to case-sensitive text type
         return match ($this->identify($grammar)) {
             'mysql' => "FIND_IN_SET({$str}, {$strList}) > 0",
-            'pgsql', 'sqlite','sqlsrv' => "({$strList} like {$str} or {$strList} like concat({$str},',%') or {$strList} like concat('%,',{$str},',%') or {$strList} like concat('%,',{$str}))",
+            'pgsql', 'sqlite' => "({$strList} like {$str} or {$strList} like (({$str})||',%') or {$strList} like ('%,'||({$str})||',%') or {$strList} like ('%,'||({$str})))",
+            'sqlsrv' => "({$strList} like {$str} or {$strList} like concat({$str},',%') or {$strList} like concat('%,',{$str},',%') or {$strList} like concat('%,',{$str}))",
         };
     }
 }
--- tests/Function/Comparison/StrListContainsTest.php
+++ tests/Function/Comparison/StrListContainsTest.php
@@ -11,8 +11,8 @@ it('can check for existence of a column within a column string list')
         'sqlsrv' => ['position' => 'where'],
     ])
     ->toBeMysql('FIND_IN_SET(`needle`, `haystack`) > 0')
-    ->toBePgsql("(\"haystack\" like \"needle\" or \"haystack\" like concat(\"needle\",',%') or \"haystack\" like concat('%,',\"needle\",',%') or \"haystack\" like concat('%,',\"needle\"))")
-    ->toBePgsql("(\"haystack\" like \"needle\" or \"haystack\" like concat(\"needle\",',%') or \"haystack\" like concat('%,',\"needle\",',%') or \"haystack\" like concat('%,',\"needle\"))")
+    ->toBePgsql("(\"haystack\" like \"needle\" or \"haystack\" like ((\"needle\")||',%') or \"haystack\" like ('%,'||(\"needle\")||',%') or \"haystack\" like ('%,'||(\"needle\")))")
+    ->toBeSqlite("(\"haystack\" like \"needle\" or \"haystack\" like ((\"needle\")||',%') or \"haystack\" like ('%,'||(\"needle\")||',%') or \"haystack\" like ('%,'||(\"needle\")))")
     ->toBeSqlsrv("([haystack] like [needle] or [haystack] like concat([needle],',%') or [haystack] like concat('%,',[needle],',%') or [haystack] like concat('%,',[needle]))");
 
 it('can check for existence of an expression within an expression string list')
@@ -21,9 +21,9 @@ it('can check for existence of an expression within an expression string list')
         'sqlsrv' => ['position' => 'where'],
     ])
     ->toBeMysql("FIND_IN_SET('a', 'a,b,c') > 0")
-    ->toBePgsql("('a,b,c' like 'a' or 'a,b,c' like concat('a',',%') or 'a,b,c' like concat('%,','a',',%') or 'a,b,c' like concat('%,','a'))")
-    ->toBePgsql("('a,b,c' like 'a' or 'a,b,c' like concat('a',',%') or 'a,b,c' like concat('%,','a',',%') or 'a,b,c' like concat('%,','a'))")
-    ->toBePgsql("('a,b,c' like 'a' or 'a,b,c' like concat('a',',%') or 'a,b,c' like concat('%,','a',',%') or 'a,b,c' like concat('%,','a'))");
+    ->toBePgsql("('a,b,c' like 'a' or 'a,b,c' like (('a')||',%') or 'a,b,c' like ('%,'||('a')||',%') or 'a,b,c' like ('%,'||('a')))")
+    ->toBeSqlite("('a,b,c' like 'a' or 'a,b,c' like (('a')||',%') or 'a,b,c' like ('%,'||('a')||',%') or 'a,b,c' like ('%,'||('a')))")
+    ->toBeSqlsrv("('a,b,c' like 'a' or 'a,b,c' like concat('a',',%') or 'a,b,c' like concat('%,','a',',%') or 'a,b,c' like concat('%,','a'))");
 
 it('can check for existence of a column within an expression string list')
     ->expect(new StrListContains(new Expression("'a,b,c'"), 'needle'))
@@ -31,8 +31,8 @@ it('can check for existence of a column within an expression string list')
         'sqlsrv' => ['position' => 'where'],
     ])
     ->toBeMysql("FIND_IN_SET(`needle`, 'a,b,c') > 0")
-    ->toBePgsql("('a,b,c' like \"needle\" or 'a,b,c' like concat(\"needle\",',%') or 'a,b,c' like concat('%,',\"needle\",',%') or 'a,b,c' like concat('%,',\"needle\"))")
-    ->toBeSqlite("('a,b,c' like \"needle\" or 'a,b,c' like concat(\"needle\",',%') or 'a,b,c' like concat('%,',\"needle\",',%') or 'a,b,c' like concat('%,',\"needle\"))")
+    ->toBePgsql("('a,b,c' like \"needle\" or 'a,b,c' like ((\"needle\")||',%') or 'a,b,c' like ('%,'||(\"needle\")||',%') or 'a,b,c' like ('%,'||(\"needle\")))")
+    ->toBeSqlite("('a,b,c' like \"needle\" or 'a,b,c' like ((\"needle\")||',%') or 'a,b,c' like ('%,'||(\"needle\")||',%') or 'a,b,c' like ('%,'||(\"needle\")))")
     ->toBeSqlsrv("('a,b,c' like [needle] or 'a,b,c' like concat([needle],',%') or 'a,b,c' like concat('%,',[needle],',%') or 'a,b,c' like concat('%,',[needle]))");
 
 it('can check for existence of an expression within a column string list')
@@ -41,6 +41,6 @@ it('can check for existence of an expression within a column string list')
         'sqlsrv' => ['position' => 'where'],
     ])
     ->toBeMysql("FIND_IN_SET('a', `haystack`) > 0")
-    ->toBePgsql("(\"haystack\" like 'a' or \"haystack\" like concat('a',',%') or \"haystack\" like concat('%,','a',',%') or \"haystack\" like concat('%,','a'))")
-    ->toBeSqlite("(\"haystack\" like 'a' or \"haystack\" like concat('a',',%') or \"haystack\" like concat('%,','a',',%') or \"haystack\" like concat('%,','a'))")
+    ->toBePgsql("(\"haystack\" like 'a' or \"haystack\" like (('a')||',%') or \"haystack\" like ('%,'||('a')||',%') or \"haystack\" like ('%,'||('a')))")
+    ->toBeSqlite("(\"haystack\" like 'a' or \"haystack\" like (('a')||',%') or \"haystack\" like ('%,'||('a')||',%') or \"haystack\" like ('%,'||('a')))")
     ->toBeSqlsrv("([haystack] like 'a' or [haystack] like concat('a',',%') or [haystack] like concat('%,','a',',%') or [haystack] like concat('%,','a'))");
