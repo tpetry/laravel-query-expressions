c76f9af76429ab2b0b5cbb606a74f08b405d1f60
Günther Debrauwer <gunther@nextapps.be>
2023-08-31 20:11:09 +0200

    wip

--- tests/Function/Time/ConvertTimezoneTest.php
+++ tests/Function/Time/ConvertTimezoneTest.php
@@ -7,7 +7,7 @@ use Tpetry\QueryExpressions\Function\Time\ConvertTimezone;
 use Tpetry\QueryExpressions\Function\Time\Now;
 
 it('can convert timezone of datetime expression with timezone expressions')
-    ->expect(new ConvertTimezone(new Expression('2023-01-01 00:00:00'), new Expression('UTC'), new Expression('Europe/Brussels')))
+    ->expect(new ConvertTimezone(new Expression('2023-01-01 00:00:00'), new Expression('\'UTC\''), new Expression('\'Europe/Brussels\'')))
     ->toBeExecutable()
     ->toBeMysql('convert_tz(\'2023-01-01 00:00:00\',\'UTC\',\'Europe/Brussels\')')
     ->toBePgsql('((\'2023-01-01 00:00:00\' at time zone \'UTC\') at time zone \'Europe/Brussels\')')
@@ -17,7 +17,6 @@ it('can convert timezone of datetime expression with timezone expressions')
 it('can convert timezone of datetime column with timezone columns')
     ->expect(new ConvertTimezone('val', 'start_timezone', 'target_timezone'))
     ->toBeExecutable(['val datetime', 'start_timezone varchar(255)', 'target_timezone varchar(255)'])
-    ->toBeExecutable()
     ->toBeMysql('convert_tz(`val`,`start_timezone`,`target_timezone`)')
     ->toBePgsql('(("val" at time zone "start_timezone") at time zone "target_timezone")')
     ->toBePgsql('(([val] at time zone [start_timezone]) at time zone [target_timezone])')
