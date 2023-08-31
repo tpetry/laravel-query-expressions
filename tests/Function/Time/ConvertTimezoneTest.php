b166d5e1581674b50992f9c88c0fe8a522b63347
Günther Debrauwer <gunther@nextapps.be>
2023-08-31 20:14:49 +0200

    wip

--- tests/Function/Time/ConvertTimezoneTest.php
+++ tests/Function/Time/ConvertTimezoneTest.php
@@ -7,7 +7,7 @@ use Tpetry\QueryExpressions\Function\Time\ConvertTimezone;
 use Tpetry\QueryExpressions\Function\Time\Now;
 
 it('can convert timezone of datetime expression with timezone expressions')
-    ->expect(new ConvertTimezone(new Expression('2023-01-01 00:00:00'), new Expression('\'UTC\''), new Expression('\'Europe/Brussels\'')))
+    ->expect(new ConvertTimezone(new Expression('\'2023-01-01 00:00:00\''), new Expression('\'UTC\''), new Expression('\'Europe/Brussels\'')))
     ->toBeExecutable()
     ->toBeMysql('convert_tz(\'2023-01-01 00:00:00\',\'UTC\',\'Europe/Brussels\')')
     ->toBePgsql('((\'2023-01-01 00:00:00\' at time zone \'UTC\') at time zone \'Europe/Brussels\')')
