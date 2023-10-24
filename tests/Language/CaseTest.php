00a9365504338993222e52bccdc01d1f7b01ab47
Michael Lundbøl <miclun@mxney.io>
2023-10-25 01:31:32 +0200

    Fix and add one more test

--- tests/Language/CaseTest.php
+++ tests/Language/CaseTest.php
@@ -9,16 +9,31 @@ use Tpetry\QueryExpressions\Language\CaseCondition;
 use Tpetry\QueryExpressions\Operator\Comparison\Equal;
 use Tpetry\QueryExpressions\Value\Number;
 
-it('use case blocks')
+it('uses a caseBlock with one condition blocks')
     ->expect(
         new CaseBlock(when: [
-            new CaseCondition(result: new Number(5), condition: new Equal(new Number(1), new Number(2)))
+            new CaseCondition(result: new Number(5), condition: new Equal(new Number(1), new Number(2))),
         ],
         else: new Number(2)
         )
     )
     ->toBeExecutable()
-    ->toBeMysql('(case when 1 = 2 then 5 end)')
-    ->toBePgsql('(case when 1 = 2 then 5 end)')
-    ->toBeSqlite('(case when 1 = 2 then 5 end)')
-    ->toBeSqlsrv('(case when 1 = 2 then 5 end)');
+    ->toBeMysql('(case when (1 = 2) then 5 end)')
+    ->toBePgsql('(case when (1 = 2) then 5 end)')
+    ->toBeSqlite('(case when (1 = 2) then 5 end)')
+    ->toBeSqlsrv('(case when (1 = 2) then 5 end)');
+
+it('uses a caseBlock with multiple condition blocks')
+    ->expect(
+        new CaseBlock(when: [
+            new CaseCondition(result: new Number(5), condition: new Equal(new Number(1), new Number(2))),
+            new CaseCondition(result: new Number(6), condition: new Equal(new Number(2), new Number(2)))
+        ],
+            else: new Number(2)
+        )
+    )
+    ->toBeExecutable()
+    ->toBeMysql('(case when (1 = 2) then 5 when (2 = 2) then 5 end)')
+    ->toBePgsql('(case when (1 = 2) then 5 when (2 = 2) then 5 end)')
+    ->toBeSqlite('(case when (1 = 2) then 5 when (2 = 2) then 5 end)')
+    ->toBeSqlsrv('(case when (1 = 2) then 5 when (2 = 2) then 5 end)');
