81ab9f0c86a06966476005070126784232f7df95
Mior Muhammad Zaki <crynobone@gmail.com>
2024-03-21 12:08:01 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,14 +10,4 @@ use Orchestra\Testbench\TestCase as Orchestra;
 class TestCase extends Orchestra
 {
     use DatabaseMigrations;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        // The RefreshDatabase trait did not work for some reason. As I don't have to run any migrations, the simplified
-        // approach of dropping tables is even better. Less work to do when setting up the test environments results in
-        // better performance.
-        // $this->getConnection()->getSchemaBuilder()->dropAllTables();
-    }
 }
