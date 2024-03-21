52c64e0915381f33ff8e18f2e8725710eb6c0926
Mior Muhammad Zaki <crynobone@gmail.com>
2024-03-21 13:58:42 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Tpetry\QueryExpressions\Tests;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    use DatabaseMigrations;
+    use LazilyRefreshDatabase;
 }
